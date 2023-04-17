b3948b3435521318b94f2c99d98afce8044453fa
hanshuliang <octopus_truth@163.com>
2023-04-17 16:34:35 +0800

    update

--- app/src/main/java/kim/hsl/webviewdemo/MainActivity.kt
+++ app/src/main/java/kim/hsl/webviewdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package kim.hsl.webviewdemo
 import android.net.http.SslError
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
@@ -33,8 +34,11 @@ class MainActivity : AppCompatActivity() {
         // 获取并设置 Web 设置
         val settings = webview.settings
         settings.javaScriptEnabled = true   // 支持 JavaScript
-        settings.domStorageEnabled = true   // 支持 HTML5
-        settings.builtInZoomControls = true // 自选 非必要
+        // 设置是否启用 DOM 存储
+        // DOM 存储是一种在 Web 应用程序中存储数据的机制，它使用 JavaScript 对象和属性来存储和检索数据
+        settings.domStorageEnabled = true
+        // 设置 WebView 是否启用内置缩放控件 ( 自选 非必要 )
+        settings.builtInZoomControls = true
 
         // 5.0 以上需要设置允许 http 和 https 混合加载
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -45,12 +49,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         // 设置页面自适应
+        // Viewport 元标记是指在 HTML 页面中的 <meta> 标签 , 可以设置网页在移动端设备上的显示方式和缩放比例
+        // 设置是否支持 Viewport 元标记的宽度
         settings.useWideViewPort = true
+
+        // 设置 WebView 是否使用宽视图端口模式
+        // 宽视图端口模式下 , WebView 会将页面缩小到适应屏幕的宽度
+        // 没有经过移动端适配的网页 , 不要启用该设置
         settings.loadWithOverviewMode = true
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             // JavaScript 出错不报异常
             try {
+                // 启用 调试模式
+                // 由于 WebView#setWebContentsDebuggingEnabled 函数不能直接访问
+                // 必须使用反射进行访问
                 val method = Class.forName("android.webkit.WebView")
                     .getMethod("setWebContentsDebuggingEnabled", java.lang.Boolean.TYPE)
                 if (method != null) {
@@ -62,13 +75,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        // 设置 WebView 是否可以获取焦点
-        webview.isFocusable = true              // 自选 非必要
-        // 设置 WebView 是否启用绘图缓存 位图缓存可加速绘图过程
-        webview.isDrawingCacheEnabled = true    // 自选 非必要
-        // 设置 WebView 中的滚动条样式
+        // 设置 WebView 是否可以获取焦点 ( 自选 非必要 )
+        webview.isFocusable = true
+        // 设置 WebView 是否启用绘图缓存 位图缓存可加速绘图过程 ( 自选 非必要 )
+        webview.isDrawingCacheEnabled = true
+        // 设置 WebView 中的滚动条样式 ( 自选 非必要 )
         // SCROLLBARS_INSIDE_OVERLAY - 在内容上覆盖滚动条 ( 默认 )
-        webview.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY // 自选 非必要
+        webview.scrollBarStyle = View.SCROLLBARS_INSIDE_OVERLAY
 
         // WebChromeClient 是一个用于处理 WebView 界面交互事件的类
         webview.webChromeClient = object : WebChromeClient() {
@@ -92,6 +105,7 @@ class MainActivity : AppCompatActivity() {
         // WebViewClient 是一个用于处理 WebView 页面加载事件的类
         webview.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                Log.i("MainActivity", "访问地址 : $url")
                 // 4.0 之后必须添加该设置
                 // 只能加载 http:// 和 https:// 页面 , 不能加载其它协议链接
                 if (url.startsWith("http://") || url.startsWith("https://")) {
